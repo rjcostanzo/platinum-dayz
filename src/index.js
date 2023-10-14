f598a6d71576244505dc3834fcd2f023537a7276
rjcostanzo <rjcostanzo91@gmail.com>
2023-10-14 14:06:40 -0500

    fix for site not rendering when deployed thru GH pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+        <React.StrictMode>
+                <App />
+        </React.StrictMode>
+);
 
-serviceWorker.unregister();
+reportWebVitals();
