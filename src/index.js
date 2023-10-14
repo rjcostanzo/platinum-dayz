315359d8a81010d6f585761f4cbec25aaf70f9ed
rjcostanzo <rjcostanzo91@gmail.com>
2023-10-14 14:11:21 -0500

    homepage was not actually fixed, undid changes to index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from './serviceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-        <React.StrictMode>
-                <App />
-        </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-reportWebVitals();
+serviceWorker.unregister();
