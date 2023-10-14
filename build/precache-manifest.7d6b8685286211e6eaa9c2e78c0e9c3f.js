a61493261cfc4807b43dd202c90b49631d26e079
rjcostanzo <rjcostanzo91@gmail.com>
2023-10-14 14:29:02 -0500

    dependency issue and homepage fix again

---
+++ build/precache-manifest.7d6b8685286211e6eaa9c2e78c0e9c3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7df3a318a704ae0d2c8",
+    "url": "/platinum-dayz/static/css/main.d3c33982.chunk.css"
+  },
+  {
+    "revision": "c7df3a318a704ae0d2c8",
+    "url": "/platinum-dayz/static/js/main.c7df3a31.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/platinum-dayz/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "56ab0b756ddce2ae58d8",
+    "url": "/platinum-dayz/static/js/runtime~main.56ab0b75.js"
+  },
+  {
+    "revision": "e2950e1af7c5833e7c1d217ab93f5d32",
+    "url": "/platinum-dayz/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rjcostanzo/platinum-site/precache-manifest.d71f5fff4de0d83009195edd0196e9e1.js"
+  "/platinum-dayz/precache-manifest.7d6b8685286211e6eaa9c2e78c0e9c3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rjcostanzo/platinum-site/index.html", {
+workbox.routing.registerNavigationRoute("/platinum-dayz/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
