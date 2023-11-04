cfe5c94751ee86bd23bc0b8e42118fc8290912b2
rjcostanzo <rjcostanzo91@gmail.com>
2023-11-03 20:19:42 -0500

    Updates

---
+++ precache-manifest.124da591eae5ed24b63d21c600c5d2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d1b0433dbaa24263ba",
+    "url": "/platinum-dayz/static/css/main.d3c33982.chunk.css"
+  },
+  {
+    "revision": "56d1b0433dbaa24263ba",
+    "url": "/platinum-dayz/static/js/main.56d1b043.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/platinum-dayz/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "56ab0b756ddce2ae58d8",
+    "url": "/platinum-dayz/static/js/runtime~main.56ab0b75.js"
+  },
+  {
+    "revision": "164ae2559cf6a0abac48c322f9ab0ce7",
+    "url": "/platinum-dayz/index.html"
+  }
+];
